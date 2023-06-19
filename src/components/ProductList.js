e5f42aad241820d5404fe8976d25fb827ef55387
Eckson-Glitho <g.eckson03@gmail.com>
2023-06-19 14:41:36 +0000

    Feature : product list layout

---
+++ src/components/ProductList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button, Table } from 'react-bootstrap';
+
+const ProductList = ({ products, onViewDetails, onEdit, onDelete }) => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Prix</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>
+              <Button variant="primary" onClick={() => onViewDetails(product.id)}>
+                Détails
+              </Button>{' '}
+              <Button variant="warning" onClick={() => onEdit(product.id)}>
+                Modifier
+              </Button>{' '}
+              <Button variant="danger" onClick={() => onDelete(product.id)}>
+                Supprimer
+              </Button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default ProductList;
