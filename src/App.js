1478359f01442ae4d6bfa8f40294ea7b874796ec
Eckson-Glitho <g.eckson03@gmail.com>
2023-06-19 15:19:22 +0000

    Feature : App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Connexion from './pages/Connexion';
-import Inscription from './pages/Inscription'
-import NavigationBar from './layouts/Navbar';
-import Produits from './pages/Produits';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import ProductList from './components/ProductList';
+import ProductDetails from './components/ProductDetails';
+import ProductForm from './components/ProductForm';
 
-function App() {
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/products');
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  const handleViewDetails = (productId) => {
+    const product = products.find((p) => p.id === productId);
+    setSelectedProduct(product);
+  };
+
+  const handleEdit = (productId) => {
+    // Implement edit functionality here
+    console.log('Edit product:', productId);
+  };
+
+  const handleDelete = (productId) => {
+    // Implement delete functionality here
+    console.log('Delete product:', productId);
+  };
+
+  const handleSubmit = async (productData) => {
+    try {
+      const response = await axios.post('http://localhost:5000/products', productData);
+      setProducts([...products, response.data]);
+    } catch (error) {
+      console.error('Error creating product:', error);
+    }
+  };
+
   return (
-    <BrowserRouter>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/inscription' element={<Inscription />} />
-        <Route path='/connexion' element={<Connexion />} />
-        <Route path='/produits' element={<Produits />} />
-      </Routes>
-    </BrowserRouter>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <h2>Liste des produits</h2>
+          <ProductList
+            products={products}
+            onViewDetails={handleViewDetails}
+            onEdit={handleEdit}
+            onDelete={handleDelete}
+          />
+        </Col>
+        <Col md={6}>
+          <h2>Détails du produit</h2>
+          {selectedProduct ? (
+            <ProductDetails product={selectedProduct} />
+          ) : (
+            <p>Sélectionnez un produit pour voir les détails.</p>
+          )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>Ajouter un produit</h2>
+          <ProductForm onSubmit={handleSubmit} />
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
